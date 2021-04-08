1308f9e43ca40cd6926e21dd3635a3fe4fabb876
Mark Underwood <mark.t.underwood@protonmail.com>
2021-04-08 13:44:10 -0800

    Add slightly broken LSTM autoencoder

---
+++ lstm.py
@@ -0,0 +1,36 @@
+# lstm autoencoder recreate sequence
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import RepeatVector
+from tensorflow.keras.layers import TimeDistributed
+from tensorflow.keras.utils import plot_model
+import pickle
+
+CHARSET = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú']
+
+data = pickle.load(open("float.pickle", "rb"))
+# define input sequence
+sequence = data[0]
+# reshape input into [samples, timesteps, features]
+n_in = len(sequence)
+sequence = sequence.reshape((1, n_in, 1))
+# define model
+model = Sequential()
+model.add(LSTM(100, activation='relu', input_shape=(n_in,1)))
+model.add(RepeatVector(n_in))
+model.add(LSTM(100, activation='relu', return_sequences=True))
+model.add(TimeDistributed(Dense(1)))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(sequence, sequence, epochs=300, verbose=0)
+plot_model(model, show_shapes=True, to_file='reconstruct_lstm_autoencoder.png')
+# demonstrate recreation
+yhat = model.predict(sequence, verbose=0)
+
+sequence = np.round(sequence * len(CHARSET))
+yhat = np.abs(np.round(yhat * len(CHARSET)))
+
+print(sequence)
+print(yhat)
