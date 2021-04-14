443286dcf2c3a22714dfbdf1d70235bbaa1d42af
Mark Underwood <mark.t.underwood@protonmail.com>
2021-04-13 23:07:59 -0800

    Add start character to charset

--- filters.py
+++ filters.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
-CHARSET = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú']
+CHARSET = [' ', '#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú']
 
 onehot_labels = OneHotEncoder(sparse=False)
 onehot_labels = onehot_labels.fit_transform(list([i] for i in range(len(CHARSET))))
--- lstm.py
+++ lstm.py
@@ -6,7 +6,7 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import OneHotEncoder
 import pickle
 
-CHARSET = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú']
+CHARSET = [' ', '#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú']
 
 onehot_labels = OneHotEncoder(sparse=False)
 onehot_labels = onehot_labels.fit_transform(list([i] for i in range(len(CHARSET))))
