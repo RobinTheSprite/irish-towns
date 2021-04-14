fddb691683455c4b8ebb041382d729aab005fb29
Mark Underwood <mark.t.underwood@protonmail.com>
2021-04-13 21:55:55 -0800

    Make LSTM model that predicts next 50 chars

--- lstm.py
+++ lstm.py
@@ -1,36 +1,74 @@
 # lstm autoencoder recreate sequence
 import numpy as np
+import sys
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import RepeatVector
-from tensorflow.keras.layers import TimeDistributed
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Dropout
+from sklearn.preprocessing import OneHotEncoder
 import pickle
 
 CHARSET = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú']
 
-data = pickle.load(open("float.pickle", "rb"))
+onehot_labels = OneHotEncoder(sparse=False)
+onehot_labels = onehot_labels.fit_transform(list([i] for i in range(len(CHARSET))))
+
 # define input sequence
-sequence = data[0]
+data = pickle.load(open("irish-towns-ngram-training-data.pickle", "rb"))
+labels = pickle.load(open("irish-towns-ngram-training-labels.pickle", "rb"))
+
 # reshape input into [samples, timesteps, features]
-n_in = len(sequence)
-sequence = sequence.reshape((1, n_in, 1))
-# define model
-model = Sequential()
-model.add(LSTM(100, activation='relu', input_shape=(n_in,1)))
-model.add(RepeatVector(n_in))
-model.add(LSTM(100, activation='relu', return_sequences=True))
-model.add(TimeDistributed(Dense(1)))
-model.compile(optimizer='adam', loss='mse')
-# fit model
-model.fit(sequence, sequence, epochs=300, verbose=0)
+n_in = data.shape[1]
+sequence = data.reshape((data.shape[0], n_in, 1))
 
-# demonstrate recreation
-yhat = model.predict(sequence, verbose=0)
+def define_model():
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(n_in,1), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(100))
+    model.add(Dropout(0.2))
+    model.add(Dense(len(CHARSET), activation='softmax'))
 
-sequence = np.round(sequence * len(CHARSET))
-yhat = np.abs(np.round(yhat * len(CHARSET)))
+    return model
 
-print(sequence)
-print(yhat)
+
+def train():
+    model = define_model()
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    # fit model
+    model.fit(sequence, labels, epochs=100, batch_size=64)
+    model.save_weights("lstm-weights.hdf5", overwrite=True)
+
+
+# from https://github.com/fchollet/deep-learning-with-python-notebooks/blob/master/8.1-text-generation-with-lstm.ipynb
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    preds = preds.reshape((32,))
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+def generate():
+    model = define_model()
+    model.load_weights("lstm-weights.hdf5")
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    name = "lis"
+    seed = name
+    for _ in range(50):
+        x = np.array(list(CHARSET.index(char) / len(CHARSET) for char in seed))
+        x = x.reshape((1, len(x), 1))
+
+        prediction = model.predict(x)
+        result = CHARSET[sample(prediction, 0.5)]
+        name += result
+
+        seed += result
+        seed = seed[1:]
+
+    print(name)
+
+train()
+generate()
